0c11ff8e319ebcf7e49092adf74e85ba2d717f31
M Faby Rizky K <fabyrizky@gmail.com>
2025-07-16 04:12:39 +0700

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,35 @@
+import React, { createContext, useContext, useReducer } from 'react';
+import appReducer from './appReducer';
 
+const AppContext = createContext();
+
+const initialState = {
+  userProfile: {
+    profession: '',
+    culturalBackground: '',
+    academicBackground: '',
+    interests: ''
+  },
+  matches: [],
+  loading: false,
+  error: null,
+  aiInsights: null
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useApp = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useApp must be used within an AppProvider');
+  }
+  return context;
+};
